21efc5eee73af73095936e7b51a80f3c7a31c1f3
Olliemattinson <olithomat@gmail.com>
2022-10-18 12:42:21 +0100

    Fixed incorrect output bug. Added results output using both Independent Filtering and Manual Cut-off methods

--- RNA_seq_read_quant_DESeq2.R
+++ RNA_seq_read_quant_DESeq2.R
@@ -20,6 +20,7 @@ runs <- colnames(countsDF)
 metadataDF <- data.frame(run=runs)
 metadataDF$condition <- sapply(strsplit(metadataDF$run,'_'),'[',1)
 conditions <- unique(metadataDF$condition)
+comparisonN <- length(conditions)
 print(metadataDF)
 
 # Provide counts data and metadata file to DDSeq
@@ -30,11 +31,11 @@ dds <- DESeqDataSetFromMatrix(countData=countsMatrix,
 
 # Define summary statistics function
     # Prints summary statistics after application of results filters
-summary_statistics <- function(res, filter) {
+summary_statistics <- function(condition1, condition2, res, filter) {
   number_DE_genes <- sum(res$padj < alpha, na.rm = TRUE)
-  print(sprintf("Number of DE expressed genes for %s is:", filter))
+  print(sprintf("Number of DE expressed genes for %s vs. %s using %s is:", condition1, condition2, filter))
   print(number_DE_genes)
-  print(sprintf("Summary for results using %s:", filter))
+  print(sprintf("Summary for %s vs. %s. using %s:", condition1, condition2, filter))
   print(summary(res))
 }
 
@@ -43,32 +44,45 @@ summary_statistics <- function(res, filter) {
 cutoff<-2
 # Define p-value threshold (alpha)
 alpha<-0.05
+# Define p-value threshold with Bonferroni correction for number of between-condition comparisons
+alphaBF<-0.05/comparisonN
 
 # Likelihood ratio test to determine which genes show differential expression between at least 2 conditions
 ddsLRT <- DESeq(dds,test='LRT',reduced=~1)
 resLRT <- results(ddsLRT)
-ddsLRTFiltered <- ddsLRT[resLRT$baseMean>cutoff, ]
-resLRTFiltered <- results(ddsLRTFiltered, independentFiltering = FALSE)
-write.csv(as.data.frame(resLRTFiltered),file=sprintf('%s/%s_DESeq2_LRT_results.csv',outputDir,experiment))
+write.csv(as.data.frame(resLRT),file=sprintf('%s/%s_DESeq2_LRT_results.csv',outputDir,experiment))
 
 # Comparisons between all condition combinations, with multiple comparison correction
-dds <- DESeq(dds)
-for (condition1 in conditions)
+ddsPairwise <- DESeq(dds)
+
+for (i in seq(1,length(conditions)-1))
 {
-  for (condition2 in conditions)
+  for (j in seq(i+1,length(conditions)))
   {
-    if (condition1!=condition2)
-    {
-      comparison=paste(condition1,'vs',condition2,sep='_')
-      res <- results(dds,pAdjustMethod='BH',contrast=c('condition',condition1,condition2),alpha=alpha)
-      ddsFiltered <- dds[res$baseMean>cutoff,]
-      resFiltered <- results(ddsFiltered,independentFiltering=FALSE)
-      resFiltered$diff_expression <- with(resFiltered, ifelse(resFiltered$log2FoldChange>0 & resFiltered$padj<alpha, condition1,ifelse(resFiltered$log2FoldChange<0 & resFiltered$padj<alpha, condition2,'')))
-      write.csv(as.data.frame(resFiltered),file=sprintf('%s/%s_DESeq2_%s_results.csv',outputDir,experiment,comparison))
-    }
+    condition1=conditions[i]
+    condition2=conditions[j]
+    comparison=paste(condition1,'vs',condition2,sep='_')
+    
+    # Results using DESeq2 Independent Filtering (IF)
+      # i.e. automatically remove genes with baseMean below automatically determined threshold 
+      # to maximise number of higher expression genes that reach significance threshold alpha 
+      # after multiple testing
+    resPairwiseIF <- results(ddsPairwise,pAdjustMethod='BH',contrast=c('condition',condition1,condition2),alpha=alpha)
+    summary_statistics(condition1, condition2, resPairwiseIF,'Independent Filtering')
+    resPairwiseIF$diff_expression <- with(resPairwiseIF, ifelse(resPairwiseIF$log2FoldChange>0 & resPairwiseIF$padj<alpha, condition1,ifelse(resPairwiseIF$log2FoldChange<0 & resPairwiseIF$padj<alpha, condition2,'')))
+    resPairwiseIF$diff_expression_with_BF_correction <- with(resPairwiseIF, ifelse(resPairwiseIF$log2FoldChange>0 & resPairwiseIF$padj<alphaBF, condition1,ifelse(resPairwiseIF$log2FoldChange<0 & resPairwiseIF$padj<alphaBF, condition2,'')))
+    write.csv(as.data.frame(resPairwiseIF),file=sprintf('%s/%s_DESeq2_%s_results_IF.csv',outputDir,experiment,comparison))
+    
+    # Results using Manual, predefined baseMean Cut-off (MC)
+    resPairwiseMC <- results(ddsPairwise,pAdjustMethod='BH',contrast=c('condition',condition1,condition2),independentFiltering=FALSE,alpha=alpha)
+    ddsPairwiseMCFiltered <- ddsPairwise[resPairwiseMC$baseMean > cutoff,]
+    resPairwiseMCFiltered <- results(ddsPairwiseMCFiltered,pAdjustMethod='BH',contrast=c('condition',condition1,condition2),independentFiltering=FALSE)
+    summary_statistics(condition1, condition2, resPairwiseMCFiltered,'Manual Cut-off')
+    resPairwiseMCFiltered$diff_expression <- with(resPairwiseMCFiltered, ifelse(resPairwiseMCFiltered$log2FoldChange>0 & resPairwiseMCFiltered$padj<alpha, condition1,ifelse(resPairwiseMCFiltered$log2FoldChange<0 & resPairwiseMCFiltered$padj<alpha, condition2,'')))
+    resPairwiseMCFiltered$diff_expression_with_BF_correction <- with(resPairwiseMCFiltered, ifelse(resPairwiseMCFiltered$log2FoldChange>0 & resPairwiseMCFiltered$padj<alphaBF, condition1,ifelse(resPairwiseMCFiltered$log2FoldChange<0 & resPairwiseMCFiltered$padj<alphaBF, condition2,'')))
+    write.csv(as.data.frame(resPairwiseMCFiltered),file=sprintf('%s/%s_DESeq2_%s_results_MC.csv',outputDir,experiment,comparison))
   }
 }
-
 
 
 
