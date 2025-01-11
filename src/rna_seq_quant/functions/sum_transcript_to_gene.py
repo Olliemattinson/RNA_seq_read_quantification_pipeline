bc5983b3ef02732877203e25ec13ee2b62494a82
Olliemattinson <olithomat@gmail.com>
2025-01-11 10:41:55 +0000

    sum_transcript_to_gene input correction

--- src/rna_seq_quant/functions/sum_transcript_to_gene.py
+++ src/rna_seq_quant/functions/sum_transcript_to_gene.py
@@ -11,11 +11,11 @@ def sum_transcript_to_gene(input_csv: str, output_csv: str, annotation_info: str
         annotation_info (str): annotation info tsv that maps transcripts to genes
     """
     # Import dataframes
-    input_df = pd.read_csv(input, sep="\t")
+    input_df = pd.read_csv(input_csv, sep="\t")
     gene_transcript_mapping = pd.read_csv(
         annotation_info, 
         sep="\t",
-        usecols=["locusName", "transcriptName"]
+        usecols=["locusName", "transcriptName"],
     )
 
     # Merge dataframes to add gene names to expression dataframe
@@ -28,7 +28,10 @@ def sum_transcript_to_gene(input_csv: str, output_csv: str, annotation_info: str
     )
 
     # Sum transcript tpm and counts to gene
-    sum_columns = [col for col in merged_df.columns if col.endswith("counts") or col.endswith("TPM")]
+    sum_columns = [col for col in merged_df.columns if col.endswith("transcript_counts") or col.endswith("transcript_TPM")]
     grouped_df = merged_df.groupby(("locusName"))[sum_columns].sum().reset_index()
+
+    # Rename transcript columns to gene columns
+    grouped_df.columns = [col.replace("transcript_counts", "gene_counts").replace("transcript_TPM", "gene_TPM") for col in grouped_df.columns]
 
     grouped_df.to_csv(output_csv, sep="\t", index=False)
